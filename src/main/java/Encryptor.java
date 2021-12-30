ff177c8a56ddba2abd24949e36fec91c86ad2f93
errorgon <37697159+errorgon@users.noreply.github.com>
2021-12-30 05:44:56 -0500

    added test cases

--- src/main/java/Encryptor.java
+++ src/main/java/Encryptor.java
@@ -1,2 +1,100 @@
+import org.jetbrains.annotations.NotNull;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.math.BigInteger;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Objects;
+
 public class Encryptor {
+
+    public static final int AES_128 = 128;
+    public static final int AES_192 = 192;
+    public static final int AES_256 = 256;
+
+    private static final String AES_CBC_NOPADDING = "AES/CBC/NoPadding";
+
+    private static final String AES = "AES";
+
+    public static SecretKey generateNewKey(int keySize) {
+        KeyGenerator keyGenerator = null;
+        try {
+            keyGenerator = KeyGenerator.getInstance(AES);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        keyGenerator.init(keySize);
+        return keyGenerator.generateKey();
+    }
+
+    public static IvParameterSpec generateIV() {
+        byte[] iv = new byte[16];
+        new SecureRandom().nextBytes(iv);
+        return new IvParameterSpec(iv);
+    }
+
+    public static String[] encrypt(String plainText) {
+        byte[] key = generateNewKey(AES_256).getEncoded();
+        byte[] iv = generateIV().getIV();
+        byte[][] ivAndCipher = encrypt(iv, key, plainText);
+        return new String[]{byteArrayToHexString(iv), byteArrayToHexString(key), byteArrayToHexString(ivAndCipher[1])};
+    }
+
+    public static byte[][] encrypt(byte[] iv, byte[] key, String plainText) {
+        return encrypt(iv, key, plainText.getBytes());
+    }
+
+    public static byte[][] encrypt(byte[] iv, byte[] key, byte[] plainText) {
+        try {
+            IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+            SecretKeySpec secretKeySpec = new SecretKeySpec(key, AES);
+            Cipher cipher = Cipher.getInstance(AES_CBC_NOPADDING);
+            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec);
+            byte[] cipherText = cipher.doFinal(plainText);
+            return new byte[][]{iv, cipherText};
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static String decrypt(String iv, String key, String cipherText) {
+        return new String(Objects.requireNonNull(decrypt(hexStringToByteArray(iv), hexStringToByteArray(key), hexStringToByteArray(cipherText))));
+    }
+
+    public static byte[] decrypt(byte[] iv, byte[] key, byte[] cipherText) {
+        try {
+            IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+            SecretKeySpec secretKeySpec = new SecretKeySpec(key, AES);
+            Cipher cipher = Cipher.getInstance(AES_CBC_NOPADDING);
+            cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, ivParameterSpec);
+            return cipher.doFinal(cipherText);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String byteArrayToHexString(byte[] arg) {
+        try {
+            int len = arg.length * 2;
+            return String.format("%0" + len + "x", new BigInteger(1, arg));
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static byte[] hexStringToByteArray(@NotNull String input) {
+        byte[] byteArray = new byte[input.length() / 2];
+        for (int i = 0; i < byteArray.length; i++) {
+            byteArray[i] = (byte) Integer.parseInt(input.substring(2 * i, 2 * i + 2), 16);
+        }
+        return byteArray;
+    }
+
 }
--- src/test/java/EncryptorTest.java
+++ src/test/java/EncryptorTest.java
@@ -1,5 +1,69 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Assert;
+import org.junit.Test;
 
-class EncryptorTest {
+import java.util.Arrays;
+
+public class EncryptorTest {
+
+
+    @Test
+    public void encryptAndDecrypt16Test() {
+        String plainText = new String("ThisIs16CharLong");
+
+        byte[] key = Encryptor.generateNewKey(Encryptor.AES_256).getEncoded();
+        byte[] iv = Encryptor.generateIV().getIV();
+        byte[][] ivAndCipher = Encryptor.encrypt(iv, key, plainText.getBytes());
+
+        byte[] result = Encryptor.decrypt(ivAndCipher[0], key, ivAndCipher[1]);
+
+        Assert.assertEquals("ThisIs16CharLong", new String(result));
+    }
+
+    @Test
+    public void encryptAndDecrypt32Test() {
+        String plainText = new String("LongerMessageIsRoughly32CharLong");
+
+        byte[] key = Encryptor.generateNewKey(Encryptor.AES_256).getEncoded();
+        byte[] iv = Encryptor.generateIV().getIV();
+        byte[][] ivAndCipher = Encryptor.encrypt(iv, key, plainText.getBytes());
+
+        byte[] result = Encryptor.decrypt(ivAndCipher[0], key, ivAndCipher[1]);
+
+        Assert.assertEquals("LongerMessageIsRoughly32CharLong", new String(result));
+    }
+
+    @Test
+    public void decrypt32Test() {
+        String key = "d41468375496980fcee3cd5e36583f4acf30b4f070691fc81c77d9e53a45a4fa";
+        String iv = "7668591bf76e5e5a84930ca579dad07a";
+        String ciperText = "a16475bf17b24ac78180ae57e81595cfe93acb3b6466a4c16254aa50788a6c57";
+
+        Assert.assertEquals("HelloWorldHelloWorldHelloWorldHe", Encryptor.decrypt(iv, key, ciperText));
+    }
+
+    @Test
+    public void encryptStringOnlyTest() {
+        String plainText = new String("LongerMessageIsRoughly32CharLong");
+        String[] ivKeyCipher = Encryptor.encrypt(plainText);
+        String result = Encryptor.decrypt(ivKeyCipher[0], ivKeyCipher[1], ivKeyCipher[2]);
+
+        Assert.assertEquals(plainText, result);
+    }
+
+    @Test
+    public void hexStringToByteArrayTest() {
+        String input = "0B0A0901";
+        byte[] result = Encryptor.hexStringToByteArray(input);
+        Assert.assertEquals(Arrays.toString(new byte[]{11, 10, 9, 1}), Arrays.toString(result));
+    }
+
+    @Test
+    public void byteArrayToHexStringTest() {
+        byte input [] = new byte[]{11, 10, 9, 1};
+        String result = Encryptor.byteArrayToHexString(input);
+        Assert.assertEquals("0B0A0901", result.toUpperCase());
+    }
+
+
 
 }
